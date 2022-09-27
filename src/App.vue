07690709deade1649edc31664bf59bbc29c0e901
bakemonostan <ehizomokhomion@gmail.com>
2022-09-27 07:12:04 -0700

    updated

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
   <!-- disable button while isPlaying is true -->
   <button type="button" @click="start" :disabled="isPlaying">Play</button>
   <!-- the block component should show based on the delay time -->
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResult">Reaction Time: {{ score }}ms</p>
 </template>
 
 <script>
@@ -14,14 +15,21 @@ export default {
   data() {
     return {
       isPlaying: false,
+      showResult: false,
       delay: null,
+      score: null,
     };
   },
   methods: {
     start() {
-      this.delay = 2000 + Math.random() * 5000;
+      this.delay = 2000 + Math.random() * 3000;
       this.isPlaying = true;
-      console.log(this.isPlaying, this.delay);
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
     },
   },
 };
